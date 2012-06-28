04b6170d8612e667e1dd13b0938d4981e07c6a96
Robert Olendorf <drolendorf@gmail.com>
2012-06-28 16:43:42 -0600

    nearly done with the first pass at connecting drupal with the middleware tool. form is pretty much done. need some validation and a http request to the middleware.

---
+++ classes/XFDUSetup.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ *    This file is part of data_curation.
+
+ *    data_curation is free software: you can redistribute it and/or modify
+ *    it under the terms of the Apache License, Version 2.0 (See License at the
+ *    top of the directory).
+
+ *    data_curation is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ *    You should have received a copy of the Apache License, Version 2.0
+ *    along with data_curation.  If not, see <http://www.apache.org/licenses/LICENSE-2.0.html>.
+ */
+
+/**
+ * Description of XFDUSetup
+ *
+ * @author olendorf
+ * 
+ */
+class XFDUSetup {
+  /**
+   *
+   * @var float 
+   */
+  public $xmlVersion = '1.0';
+  
+  /**
+   *
+   * @var string
+   */
+  public $xmlEncoding = 'UTF-8';
+  
+  /**
+   * The version of XFDU being used.
+   * @var string 
+   */
+  public $version = '1.0';
+  
+  /**
+   * The absolute path the repository the data will reside in.
+   * @var string 
+   */
+  public $repository;
+  
+  /**
+   * Path to the data if it is to be uploaded
+   * @var string 
+   */
+  public $upload;
+  
+  /**
+   * Path to existing data to be processed.
+   * @var string 
+   */
+  public $existing;
+  
+  /**
+   *
+   *the path to the top most directory in the data set to be worked on, relative to
+   * the repository root. 
+   * @var string
+   */
+  public $root;
+  
+  /**
+   * ID for the package header. Defaults to <strong>packageHeader</strong> if left blank; 
+   * @var string 
+   */
+  public $packageHeaderID = 'packageHeader';
+  
+  /**
+   * If the XFDU package is one of a series, set this to the position of the series.
+   * @var integer 
+   */
+  public $sequencePosition;
+  
+  /**
+   * The number of packages in an XFDU series if there is one.
+   * @var integer 
+   */
+  public $sequenceSize;
+  
+  /**
+   * The text to display for sequence info should there be any.
+   * @var string
+   */
+  public $sequenceInfoText;
+  
+  /**
+   * XML data about the metadata package, ie. the curator and date metadata was created.
+   * To be included with all XFDU documents in the dataset. For more
+   * structure XML metadata use extension.
+   * @var string 
+   */
+  public $xmlData;
+  
+  /**
+   * XML data to be included int the packageHeader of all XFDU documents in the 
+   * dataset. If the XML data doesn't have a root element that is also 
+   * namespaced, the additional <strong>$extensionNamespace</strong> attribute 
+   * must be defined with the namespace. The 
+   * <strong>$extensionNamespaceLocation</strong> is optional but allows 
+   * validation.  
+   * 
+   
+   * @var string
+   */
+  public $extension;
+  
+  /**
+   * If the root element of the the <strong>$extension</strong> does not contain
+   * a namespace attribute this must be set or an exception is thrown when the 
+   * <strong>get_as_DOM()</strong> method is called.
+   * @var string 
+   */
+  public $extensionNamespace;
+  
+  /**
+   * Specify the prefix for the namespace if needed.
+   * @var string 
+   */
+  public $extensionPrefix;
+  
+  /**
+   * The location for the namespace if desired.
+   * @var string
+   */
+  public $extensionNamespaceLocation;
+  
+  /**
+   * Either a metadataReference, metadataWrap or a dataObjectPointer that
+   * gives descriptive metadata to be included in with every file in the data
+   * set. The data, should be high level and applicable to all files and 
+   * directories.
+   * @var mixed  
+   */
+  public $descriptiveMetadata;
+  
+  
+  
+}
+
+?>
--- forms/project_forms.php
+++ forms/project_forms.php
@@ -8,6 +8,7 @@
  */
 
 
+
 /**
  * Form for creating a new data curation project.
  * 
@@ -16,6 +17,8 @@
  * @return array 
  */
 function curation_tool_new_project_form($form, &$form_state) {
+  global $user;
+  $user_fields = user_load($user->uid);
   
   $form['data'] = array(
       '#type' => 'fieldset',
@@ -42,20 +45,22 @@ function curation_tool_new_project_form($form, &$form_state) {
       '#description' => t('Enter information about the curators and curatoin process here.'),
   );
   
-  $form['curator_info']['curator'][] = array(
-      '#title' => t('Curator&apos;s Name'),
+  $form['curator_info']['curator'] = array(
+      '#title' => t('Curator&apos;s Name(s)'),
       '#description' => t('The name(s) of the individuals responsible for '.
               'curating this data set. Separate names with commas.'),
       '#type' => 'textfield',
       '#length' => 60,
       '#maxlength' => 120,
-  );
+      '#required' => TRUE,
+      );
   
-  $form['curator_info']['date'] = array(
+  $form['curator_info']['metaCreateDate'] = array(
       '#title' => t('Creation Date'),
       '#type' => 'textfield',
       '#length' => 60,
       '#default_value' => date('c', time()),
+      '#required' => TRUE,
   );
   
   $form['descriptive_metadata'] = array(
@@ -85,7 +90,7 @@ function curation_tool_new_project_form($form, &$form_state) {
   );
   
   $form['descriptive_metadata']['contributor'] = array(
-      '#title' => t('Data Creators'),
+      '#title' => t('Other Contributors'),
       '#description' => t('Enter the names of other people with a role in creating the data '.
               'separated by commas.'),
       '#type' => 'textfield',
@@ -102,13 +107,14 @@ function curation_tool_new_project_form($form, &$form_state) {
       '#maxlength' => 256,
   );
   
-  $form['descriptive_metadata']['created'] = array(
+  $form['descriptive_metadata']['dataCreated'] = array(
       '#title' => t('Date Created'),
       '#description' => t('Enter the date the data was created <em>(YYYY-MM-DD)</em>'),
       '#type' => 'textfield',
       '#length' => 80,
       '#maxlength' => 256,
       '#default_value' => date('Y-m-d', time()),
+      '#required' => TRUE,
   );
   
   $form['descriptive_metadata']['abstract'] = array(
@@ -132,8 +138,8 @@ function curation_tool_new_project_form($form, &$form_state) {
   $form['license_metadata'] = array(
       '#type' => 'fieldset',
       '#title' => 'Rights and Licensing Information',
-      '#description' => 'Use this section to document any other parties can claim '.
-              'intellectual property rights over your entire dataset. If only '.
+      '#description' => 'Use this section to document licensing and rights issues '.
+              'concerning your entire dataset. If only '.
               'parts of your dataset can be claimed, you may document that specifically '.
               'for those files or groups of file later.',
   );
@@ -150,42 +156,251 @@ function curation_tool_new_project_form($form, &$form_state) {
   
   $form['license_metadata']['rights'] = array(
       '#title' => t('Rights Information'),
-      '#description' => t('If any other parties can claim rights over this data set.' ),
+      '#description' => t('If any other parties can claim rights over this data set, list them and describe the nautre of their claim.' ),
       '#type' => 'textarea',
       '#cols' => 80,
       '#rows' => 10,
       '#resizeable' => TRUE,
   );
   
-  
-  
-  $form['license_metadata']['rightsholder'] = array(
-      '#title' => t('Rights Information'),
-      '#description' => t('List the rights holders other than creators and contributors.'),
-      '#type' => 'textfield',
-      '#length' => 80,
-      '#maxlength' => 200,
+  $form['assurances'] = array(
+      '#title' => t('Assurances'),
+      '#type' => 'fieldset',
+      '#description' => 'Please read the following assurances. Each box '.
+                        'must be checked to upload the data. By checking the boxes '.
+                        'you are stating that the statements are true.',
   );
   
-  $form['action']['submit'] = array(
+  $form['assurances']['assureCopyright'] = array(
+      '#type' => 'checkbox',
+      '#description' => 'I am the owner of this content or have permission to release '.
+                        'this content to the University of New Mexico Institutional Repository (LoboVault).',
+//      '#required' => TRUE,
+  );
+  
+  $form['assurances']['releaseCopyright'] = array(
+      '#type' => 'checkbox',
+      '#description' => 'I understand that LoboVault is an open access repository and '.
+                        'that the content I am uploaded will be available to the public '.
+                        'immediately or after a specified embargo period of less than 2 years.',
+//      '#required' => TRUE,
+  );
+  
+  $form['assurances']['assureInformation'] = array(
+      '#type' => 'checkbox',
+      '#description' => 'I have supplied sufficient supporting information to make this data '.
+                        'understandable and accessable to others.',
+//      '#required' => TRUE,
+  );
+  
+  $form['submit'] = array(
       '#type' => 'submit',
       '#value' => 'Submit',
+      '#submit' => array('curation_tool_new_project_submit'),
+      '#validate' => array('curation_tool_new_project_validate'),
   );
-  
   return $form;
 }
 
 /**
- * Validate input here beyond what Drupal does.
- * @param array $form
- * @param array $form_state 
+ * @todo validate dates
+ * @todo validate assurances
+ * @todo check all other possible validations.
+ * @param type $form
+ * @param type $form_state 
  */
-function curation_tool_new_project_form_validate($form, &$form_state) {
-  
+function curation_tool_new_project_validate($form, &$form_state) {
+  if(! $form_state['values']['upload_data'] xor 
+          $form_state['values']['existing_data']) {
+    form_set_error('data', t('You must enter either the location of the '.
+            'existing data or upload data, but not both.'));
+  }
 }
 
+/**
+ * A helper funciton to add a bunch of namespaced elements to a document.
+ * @param DOMDocument $doc
+ * @param array $data
+ * @param string $namespaceURI
+ * @param string $qname
+ * @param DOMElement $parent 
+ */
+function append_elementNS(DOMDocument &$doc, array $data, $namespaceURI, $qname, DOMElement $parent = null) {
+  foreach($data as $value) {
+    if($parent) {
+      $parent->appendChild($element = $doc->createElementNS($namespaceURI, $qname));
+    }
+    else {
+      $doc->appendChild($element = $doc->createElementNS($namespaceURI, $qname));
+    }
+    $element->appendChild($doc->createTextNode($value));
+  }
+}
+
+/**
+ * @todo this is currently FUUUUGGGLLLYYY. However it works. Consider cleaning up the
+ * code.
+ * @param type $form
+ * @param type $form_state 
+ */
 function curation_tool_new_project_submit($form, &$form_state) {
+
+  drupal_set_message('IT WORKED!!!<br/>');
   
+  $settings = new XFDUSetup();  
+  
+  //Set the location of the data to be processed. Since either one or the other
+  // must be specified, we just set the value of both. This acts as a switch
+  // on the data processing system.
+  $settings->upload = $form_state['values']['upload_data'];
+  $settings->existing = $form_state['values']['existing_data'];
+  $settings->repository = variable_get('repository_root');
+  
+  $dom = new DOMDocument('1.0', 'UTF-8');
+  $dcURI = 'http://purl.org/dc/elements/1.1/';
+  $dcPrefix = 'dc';
+  $termsURI = 'http://purl.org/dc/terms/';
+  $termsPrefix = 'dcterms';
+  
+  ////////////////////////
+  // Handle the packagheader metadata
+  
+  $anyXML = $dom->appendChild($wrapper = $dom->createElement('wrapper'));
+  $anyXML->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:'.$dcPrefix, $dcURI);
+  $anyXML->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:'.$termsPrefix, $termsURI);
+ 
+
+  
+  // Handle metadata about the curators.
+  append_elementNS(
+          $dom, 
+          explode(',', $form_state['values']['curator']), 
+          $dcURI,
+          $dcPrefix.':creator',
+          $anyXML);
+  
+  // Handle the creation date for the metadata
+  append_elementNS(
+          $dom, 
+          array($form_state['values']['metaCreateDate']), 
+          $dcURI, 
+          $dcPrefix.':created', 
+          $anyXML
+          );
+  
+  $settings->xmlData = $dom->saveXML();
+  
+  /////////////////////////////////////////////
+  // Create a new dom document and handle the descriptive
+  // metadata for the data.
+  
+  $dom = new DOMDocument('1.0', 'UTF-8');
+
+  $dmd = $dom->appendChild($wrapper = $dom->createElement('wrapper'));
+  $dmd->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:'.$dcPrefix, $dcURI);
+  $dmd->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:'.$termsPrefix, $termsURI);
+  
+  // Handle the name of the data  
+  append_elementNS(
+          $dom, 
+          array($form_state['values']['title']), 
+          $dcURI, 
+          $dcPrefix.':title', 
+          $dmd
+          );
+  
+  // Handle the creators of the data.  
+  append_elementNS(
+          $dom, 
+          explode(',', $form_state['values']['creator']), 
+          $dcURI, 
+          $dcPrefix.':creator', 
+          $dmd
+          );
+  
+  // Handle contributors to the data
+  if($form_state['values']['contributor']) {
+    append_elementNS(
+            $dom, 
+            explode(',', $form_state['values']['contributor']), 
+            $dcURI, 
+            $dcPrefix.':contributor', 
+            $dmd
+            );
+    }  
+  
+  // Handle any subjects or keywords supplied
+  if($form_state['values']['subject']) {
+    append_elementNS(
+            $dom, 
+            explode(',', $form_state['values']['subject']), 
+            $dcURI, 
+            $dcPrefix.':subject', 
+            $dmd
+            );
+    }    
+  
+  // Handle any subjects or keywords supplied
+  if($form_state['values']['dataCreated']) {
+    append_elementNS(
+            $dom, 
+            array($form_state['values']['dataCreated']), 
+            $termsURI, 
+            $termsPrefix.':created', 
+            $dmd
+            );
+  }
+  
+  //Handle the data abstract.
+  if($form_state['values']['abstract']) {
+    append_elementNS(
+            $dom, 
+            array($form_state['values']['abstract']), 
+            $termsURI, 
+            $termsPrefix.':abstract', 
+            $dmd
+            );
+  }
+  
+  //Handle the description.
+  if($form_state['values']['description']) {
+    append_elementNS(
+            $dom, 
+            array($form_state['values']['description']), 
+            $dcURI, 
+            $dcPrefix.':description', 
+            $dmd
+            );
+  }
+  
+  //Handle the licensing information
+  if($form_state['values']['license']) {
+    append_elementNS(
+            $dom, 
+            array($form_state['values']['license']), 
+            $termsURI, 
+            $termsPrefix.':license', 
+            $dmd
+            );
+  }
+  
+  //Handle the rights information
+  if($form_state['values']['rights']) {
+    append_elementNS(
+            $dom, 
+            array($form_state['values']['rights']), 
+            $termsURI, 
+            $termsPrefix.':rights', 
+            $dmd
+            );
+  }
+  
+
+  
+//  drupal_set_message(base64_encode(json_encode($settings, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP)));
+  drupal_set_message(json_encode($settings, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP));
+
+  //$form_state['redirect'] = '';
 }
 
 ?>
